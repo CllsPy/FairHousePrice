3954d066e40e29a94a9a0a0bd2a58fe72096ca5f
Cllspy <ifal.edu.2017@gmail.com>
2026-01-21 00:07:55 -0300

    commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import pickle
+import streamlit as st
+
+st.set_page_config(page_title="Estimativa de Preço", layout="centered")
+
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+st.title("Estimativa de Preço do Imóvel")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    area_m2 = st.number_input("Área (m²)", min_value=0.0, step=1.0)
+    num_quartos = st.number_input("Quartos", min_value=0, step=1)
+
+with col2:
+    num_banheiros = st.number_input("Banheiros", min_value=0, step=1)
+    idade_anos = st.number_input("Idade do imóvel (anos)", min_value=0, step=1)
+
+if st.button("Calcular"):
+    pred = model.predict([[area_m2, num_quartos, num_banheiros, idade_anos]])[0]
+    st.metric("Preço estimado", f"{pred:,.2f}")
